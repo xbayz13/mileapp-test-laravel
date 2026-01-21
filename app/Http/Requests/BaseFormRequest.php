aec42558d9219e1373cbcd57705277fdb8d4fb6e
Ikhsan Pahdian <ikhsanpahdian@Ikhsans-MacBook-Air.local>
2026-01-21 18:08:39 +0700

    feat: add base form request class

---
+++ app/Http/Requests/BaseFormRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+
+abstract class BaseFormRequest extends FormRequest
+{
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param  \Illuminate\Contracts\Validation\Validator  $validator
+     * @return void
+     *
+     * @throws \Illuminate\Http\Exceptions\HttpResponseException
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'success' => false,
+                'message' => 'Validation error',
+                'errors' => $validator->errors(),
+            ], 422)
+        );
+    }
+}
