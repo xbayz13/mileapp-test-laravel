ed929b0c15e8e54f9017198df35c9a0bf939f14a
Ikhsan Pahdian <ikhsanpahdian@Ikhsans-MacBook-Air.local>
2026-01-21 18:31:04 +0700

    refactor: update AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\Contracts\TaskRepositoryInterface;
+use App\Repositories\TaskRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // Bind Repository Interface to Implementation (Dependency Inversion Principle)
+        $this->app->bind(TaskRepositoryInterface::class, TaskRepository::class);
     }
 
     /**
