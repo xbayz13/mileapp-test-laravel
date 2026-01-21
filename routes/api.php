582078a0c946d2dbfeac596939ce717718407609
Ikhsan Pahdian <ikhsanpahdian@Ikhsans-MacBook-Air.local>
2026-01-21 19:31:38 +0700

    feat: add API routes for authentication and tasks

---
+++ routes/api.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\TaskController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Public routes
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes (require JWT authentication)
+Route::middleware('auth:api')->group(function () {
+    // Auth routes
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/me', [AuthController::class, 'me']);
+
+    // Task routes
+    Route::apiResource('tasks', TaskController::class);
+});
