0d83bd8fa1b6eabab1bf8ac5396620f4a671b44a
Ikhsan Pahdian <ikhsanpahdian@Ikhsans-MacBook-Air.local>
2026-01-21 18:19:23 +0700

    feat: add Task model with MongoDB support

---
+++ app/Models/Task.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use MongoDB\Laravel\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'mongodb';
+    
+    protected $collection = 'tasks';
+
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'priority',
+        'due_date',
+        'completed_at',
+    ];
+
+    protected $casts = [
+        'due_date' => 'datetime',
+        'completed_at' => 'datetime',
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime',
+    ];
+
+    /**
+     * Get the primary key for the model.
+     *
+     * @return string
+     */
+    public function getKeyName()
+    {
+        return '_id';
+    }
+
+    /**
+     * Convert the model instance to an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        $array = parent::toArray();
+        
+        // Ensure _id is present in the array
+        if (isset($array['id']) && !isset($array['_id'])) {
+            $array['_id'] = $array['id'];
+        }
+        
+        return $array;
+    }
+}
