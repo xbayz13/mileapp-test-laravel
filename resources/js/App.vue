d16fccd870894731ee6c744ec3212d10ef05bc41
Ikhsan Pahdian <ikhsanpahdian@Ikhsans-MacBook-Air.local>
2026-01-21 21:24:16 +0700

    feat: create frontend application structure

---
+++ resources/js/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { onMounted } from 'vue'
+import { useAuthStore } from './stores/auth'
+
+const authStore = useAuthStore()
+
+onMounted(() => {
+  authStore.initAuth()
+})
+</script>
+
+<template>
+  <router-view />
+</template>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Vue.js SPA routes - catch all routes and return app view
+Route::get('/{any}', function () {
+    return view('app');
+})->where('any', '.*');
